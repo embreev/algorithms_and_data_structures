ad1ca497d65cd82a29d0a2baf91c3f8de90d525e
embreev <e.m.breev@gmail.com>
2019-04-22 02:00:25 +0300

    -

--- src/main/java/ru/breev/App.java
+++ src/main/java/ru/breev/App.java
@@ -19,33 +19,6 @@ public class App {
         fillInt();
         System.out.println(array);
 
-        tmpArray = new ArrayImpl<>(CAPACITY);
-
-        copyArray();
-        startTime = System.nanoTime();
-        tmpArray.sortBubble();
-        stopTime = System.nanoTime();
-        System.out.println("Bubble: " + TimeUnit.NANOSECONDS.toSeconds((long) (stopTime - startTime)));
-
-        copyArray();
-        startTime = System.nanoTime();
-        tmpArray.sortSelect();
-        stopTime = System.nanoTime();
-        System.out.println("Select: " + TimeUnit.NANOSECONDS.toSeconds((long) (stopTime - startTime)));
-
-        copyArray();
-        startTime = System.nanoTime();
-        tmpArray.sortInsert();
-        stopTime = System.nanoTime();
-        System.out.println("Insert: " + TimeUnit.NANOSECONDS.toSeconds((long) (stopTime - startTime)));
-
-        System.out.println(tmpArray);
-    }
-
-    private static void copyArray() {
-        for (int i = 0; i < array.size(); i++) {
-            tmpArray.add(array.get(i));
-        }
     }
 
     private static void fillInt() {
