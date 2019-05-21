26590cd1230728ac7ae81324d44ed80e5dae81c2
embreev <e.m.breev@gmail.com>
2019-05-21 22:23:41 +0300

    added class Item

---
+++ src/main/java/ru/breev/hash/Item.java
@@ -0,0 +1,33 @@
+package ru.breev.hash;
+
+public class Item {
+
+    private int id;
+    private String title;
+
+    public Item(int id, String title) {
+        this.id = id;
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
